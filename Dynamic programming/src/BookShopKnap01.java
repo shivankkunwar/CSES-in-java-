1eabf1a180efd930cea20121cbbbb0e313ed35c3
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-03-31 22:57:35 +0530

    Update BookShopKnap01.java

--- Dynamic programming/src/BookShopKnap01.java
+++ Dynamic programming/src/BookShopKnap01.java
@@ -20,26 +20,7 @@ public class BookShopKnap01 {//giving TLE
     public  static  int  Book(int []cost,int []pages,int n,int x){
         int dp[][]= new int[n+1][x+1];
 
-//        for(int i=1;i<=n;i++)
-//        {
-//            for(int j=0;j<=x;j++)
-//            {
-//                //option1 = book number i-1 is not included, hence no pages added.
-//                //option2 = book number i-1 is included, hence pages are added.
-//                //option1 = dp[i-1][j] ...book not included so total price(j) remains same.
-//                //option2 = dp[i-1][j-price[i-1]] + page[i-1]....total price(j) decreased and pages are counted for book i-1.
-//
-//                dp[i][j] = dp[i-1][j];
-//                if(j >= cost[i-1])
-//                {
-//                    dp[i][j] =Math.max (dp[i][j],dp[i-1][j-cost[i-1]] + pages[i-1]); //max of both the boxes included in dp[i][j].
-//                }
-//            }
-//        }
-//        return dp[n][x];
 
-
-//
 
         for(int i=0;i<=n;i++){
             for(int money=0;money<=x;money++){
